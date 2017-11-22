5ba5214ded83fcc0ce4fc38bf8377a348c1fdf69
neerajgarg0710 <neerajgarg0710@users.noreply.github.com>
2017-11-22 17:36:15 +0530

    Added new API-gateway project
    Added new API-gateway project

--- metallica-ref-data-service/src/main/java/com/sapient/metallica/controller/rest/RefDataService.java
+++ metallica-ref-data-service/src/main/java/com/sapient/metallica/controller/rest/RefDataService.java
@@ -2,19 +2,26 @@ package com.sapient.metallica.controller.rest;
 
 import java.util.Date;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sapient.metallica.beans.Trade;
 
 @RestController
+@SpringBootApplication
 public class RefDataService {
 
 	@RequestMapping(value = "/getTradeDetails")
-    public Trade getStudentDetails(@PathVariable(name = "name") String name)
+    public Trade getStudentDetails()
     {
         return new Trade(10, 1000d, new Date());
+    }
+	
+	public static void main(String[] args)
+    {
+        SpringApplication.run(RefDataService.class, args);
     }
 	
 }
