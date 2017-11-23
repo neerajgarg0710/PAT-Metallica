2c0e90ca1a6eb64b34c4512ddff873e88ab1d662
neerajgarg0710 <neerajgarg0710@users.noreply.github.com>
2017-11-23 08:08:13 +0530

    App bar and Tabs for Mettalica App
    App bar and Tabs for Mettalica App

--- metallica-ui/App.jsx
+++ metallica-ui/App.jsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Avatar from 'material-ui/Avatar';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
+const style = {margin: 25};
+const labelStyle = {color: 'white'};
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
+
+const Logged = (props) => (
+	<div>
+	<span style={labelStyle}>UserName</span>
+	<Avatar size={30} style={style}>
+	  A
+	</Avatar>
+	</div>
+);
+
+Logged.muiName = 'IconMenu';
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+  render() {
+    return (
+	<MuiThemeProvider>
+      <div>
+        <AppBar
+          title="Metallica App"
+          iconElementRight={<Logged />}
+        />
+      </div>
+	  <div>
+	  
+	  <Tabs style={}>
+    <Tab label="Trades" >
+      <div>
+        <h2 style={styles.headline}>Trades</h2>
+      </div>
+    </Tab>
+	<Tab label="Transfer" >
+      <div>
+        <h2 style={styles.headline}>Transfer</h2>
+      </div>
+    </Tab>
+	<Tab label="Transport" >
+      <div>
+        <h2 style={styles.headline}>Transport</h2>
+      </div>
+    </Tab>
+	</Tabs>
+	  
+	  
+	  </div>
+	  </MuiThemeProvider>
+    );
+  }
 }
+
 export default App;
