3f97bccc9a31b4a4b028946aad538a92f56dc0cf
BhuvnSha <33890439+BhuvnSha@users.noreply.github.com>
2017-11-25 13:02:39 +0530

    RabbitMQ

--- metallica-ref-data-service/src/main/java/com/sapient/metallica/MetallicaServiceApplication.java
+++ metallica-ref-data-service/src/main/java/com/sapient/metallica/MetallicaServiceApplication.java
@@ -1,7 +1,21 @@
 package com.sapient.metallica;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sapient.metallica.message.Receiver;
+
+
 
 
 @SpringBootApplication
@@ -11,4 +25,34 @@ public class MetallicaServiceApplication {
 		SpringApplication.run(MetallicaServiceApplication.class, args);
 	}
 
+	public static final String queueName="metallica-queue";
+	
+	@Bean
+	Queue queue(){
+		return new Queue(queueName, false);
+	}
+	
+	@Bean
+	TopicExchange exchange(){
+		return new TopicExchange("metallica-exchange");
+	}
+	
+	@Bean 
+	Binding binding(Queue queue, TopicExchange exchange){
+		return BindingBuilder.bind(queue).to(exchange).with(queueName);
+	}
+	
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListener messageListener){
+		SimpleMessageListenerContainer msgContainer = new SimpleMessageListenerContainer();
+		msgContainer.setConnectionFactory(connectionFactory);
+		msgContainer.setMessageListener(messageListener);
+		msgContainer.setQueueNames(queueName);
+		return msgContainer;		
+	}
+	
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver){
+		return new MessageListenerAdapter(receiver, "receiveMessage");			
+	}
 }
